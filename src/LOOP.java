073b7246564c602204538feb12e3a1681a9dfa79
sonu choure <131000134+choure123@users.noreply.github.com>
2023-06-01 20:51:25 +0530

    ADDED FORLOOP

--- src/LOOP.java
+++ src/LOOP.java
@@ -107,3 +107,11 @@ class CompareStrings {
             System.out.println("Both strings are equal.");
     }
 }
+class ForLoop {
+    public static void main(String[] args) {
+        int c;
+        for (c = 1; c <= 10; c++) {
+            System.out.println(c);
+        }
+    }
+}
