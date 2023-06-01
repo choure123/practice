aacab34a1fb0d8df6caa94645151d78d361ec144
sonu choure <131000134+choure123@users.noreply.github.com>
2023-06-01 18:26:00 +0530

    ADDED CALCULATOR

--- src/INTEGER.java
+++ src/INTEGER.java
@@ -1,3 +1,5 @@
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingParameterStyle;
+
 import java.util.Scanner;
 
 public class INTEGER {
@@ -81,5 +83,84 @@ class Swapnumbers1 {
 
         System.out.println(" before Swappping \nx = +x+\ny = " + y);
 
+    }
+}
+class ADDNUMBER {
+    public static void main(String[] args) {
+        int X, Y, Z;
+        System.out.println(" enter two interger to calculate their sum ");
+        Scanner in = new Scanner(System.in);
+        X = in.nextInt();
+        Y = in.nextInt();
+        Z = X + Y;
+        System.out.println(" SUM OF enter  INTERGER =" + Z);
+
+
+    }
+}
+class subtraction {
+    public static void main(String[] args) {
+        int X, Y, Z;
+        System.out.println(" enter two interger to calculate their sub ");
+        Scanner in = new Scanner(System.in);
+        X = in.nextInt();
+        Y = in.nextInt();
+        Z = X - Y;
+        System.out.println(" SUM OF enter  INTERGER =" + Z);
+
+
+    }
+}
+
+ class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select an operation : ");
+        System.out.println("1. Addition (-)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("The sum is: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("The difference is: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("The product is: " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("The quotient is: " + result);
+                } else {
+                    System.out.println("Error: Cannot divide by zero!");
+                }
+                break;
+
+            default:
+                System.out.println("Error: Invalid operator!");
+                break;
+        }
+
+        scanner.close();
     }
 }
