74a0f7dcda590f9bfc86251370c189cf8a7437d9
sonu choure <131000134+choure123@users.noreply.github.com>
2023-06-01 16:42:01 +0530

    ADDED SAMPLE PROGRAM

---
+++ src/INTEGER.java
@@ -0,0 +1,16 @@
+public class INTEGER {
+    public static void main(String[] args) {
+        int C; // declaring  a variable
+        for ( C=1 ; C<=100 ; C++);
+        System.out.println(C);
+
+    }
+}
+ class ARGUMENTS{
+     public static void main(String[] args) {
+         for (String t: args) {
+             System.out.println( t);
+
+         }
+     }
+ }
