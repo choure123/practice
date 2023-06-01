d4db9b2b68fccb667b13f64eb66b91f8abc8cb18
sonu choure <131000134+choure123@users.noreply.github.com>
2023-06-01 23:08:34 +0530

    ADDED OOPSPROBLEM

--- src/OOPs.java
+++ src/OOPs.java
@@ -1,4 +1,12 @@
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.net.InetAddress;
+
 public class OOPs {
     public static void main(String args[]) {
         String s1 = "hello";
@@ -20,7 +28,7 @@ class StringEndwith {
     }
 }
 
- class IndexOfExample {
+class IndexOfExample {
     public static void main(String args[]) {
         String s1 = "this is index of example";
 //passing substring
@@ -36,77 +44,86 @@ class StringEndwith {
         System.out.println(index4);//3
     }
 }
- class IndexOfExample2{
-    public static void main(String args[]){
-        String s1="this is index of example";
+
+class IndexOfExample2 {
+    public static void main(String args[]) {
+        String s1 = "this is index of example";
 //passing substring
-        int index1=s1.indexOf("is");//returns the index of is substring
-        int index2=s1.indexOf("index");//returns the index of index substring
-        System.out.println(index1+" "+index2);//2 8
+        int index1 = s1.indexOf("is");//returns the index of is substring
+        int index2 = s1.indexOf("index");//returns the index of index substring
+        System.out.println(index1 + " " + index2);//2 8
 //passing substring with from index
-        int index3=s1.indexOf("is",4);//returns the index of is substring after 4th index
+        int index3 = s1.indexOf("is", 4);//returns the index of is substring after 4th index
         System.out.println(index3);//5 i.e. the index of another is
 //passing char value
-        int index4=s1.indexOf('s');//returns the index of s char value
+        int index4 = s1.indexOf('s');//returns the index of s char value
         System.out.println(index4);//3
-    }}
+    }
+}
 
- class ReplaceAllExample2{
-    public static void main(String args[]){
-        String s1="My name is Rajendra. My name is lamror. My name is Technolamror.";
-        String replaceString=s1.replaceAll("is","was");//replaces all occurrences of "is" to"was"
+class ReplaceAllExample2 {
+    public static void main(String args[]) {
+        String s1 = "My name is Rajendra. My name is lamror. My name is Technolamror.";
+        String replaceString = s1.replaceAll("is", "was");//replaces all occurrences of "is" to"was"
         System.out.println(replaceString);
-    }}
- class SplitExample{
-    public static void main(String args[]){
-        String s1="java string split method by Technolamror";
-        String[] words=s1.split("\\s");//splits the string based on whitespace
+    }
+}
+
+class SplitExample {
+    public static void main(String args[]) {
+        String s1 = "java string split method by Technolamror";
+        String[] words = s1.split("\\s");//splits the string based on whitespace
 //using java foreach loop to print elements of string array
-        for(String w:words){
+        for (String w : words) {
             System.out.println(w);
         }
-    }}
- class StringTrimExample{
-    public static void main(String args[]){
-        String s1=" hello string ";
-        System.out.println(s1+"Technolamror");//without trim()
-        System.out.println(s1.trim()+"Technolamror");//with trim()
-    }}
+    }
+}
 
- class StringLowerExample{
-    public static void main(String args[]){
-        String s1="TECHNOLAMROR by Rajendralamror HELLO stRIng";
-        String s1lower=s1.toLowerCase();
+class StringTrimExample {
+    public static void main(String args[]) {
+        String s1 = " hello string ";
+        System.out.println(s1 + "Technolamror");//without trim()
+        System.out.println(s1.trim() + "Technolamror");//with trim()
+    }
+}
 
+class StringLowerExample {
+    public static void main(String args[]) {
+        String s1 = "TECHNOLAMROR by Rajendralamror HELLO stRIng";
+        String s1lower = s1.toLowerCase();
 
 
         System.out.println(s1lower);
-    }}
+    }
+}
+
 class Methods {
     // Constructor method
     Methods() {
         System.out.println("Constructor method is called when an object of it's class is created");
     }
+
     // Main method where program execution begins
     public static void main(String[] args) {
         staticMethod();
         Methods object = new Methods();
         object.nonStaticMethod();
     }
+
     // Static method
     static void staticMethod() {
         System.out.println("Static method can be called without creating object");
     }
+
     // Non static method
     void nonStaticMethod() {
         System.out.println("Non static method must be called by creating an object");
     }
 }
 
-class StringMethods
-{
-    public static void main(String args[])
-    {
+class StringMethods {
+    public static void main(String args[]) {
         int n;
         String s = "Java programming", t = "", u = "";
         System.out.println(s);
@@ -128,15 +145,18 @@ class StaticBlock {
     public static void main(String[] args) {
         System.out.println("Main method is executed.");
     }
+
     static {
         System.out.println("Static block is executed before main method.");
     }
 }
+
 //Static Block Application …. We need to open Program in speciif window
 class StaticBlock1 {
     public static void main(String[] args) {
         System.out.println("You are using Windows_NT operating system.");
     }
+
     static {
         String os = System.getenv("OS");
         if (os.equals("Windows_NT") != true) {
@@ -151,20 +171,25 @@ class Difference {
         Difference t = new Difference();
         t.show(); //calling using object
     }
+
     static void display() {
         System.out.println("Programming is amazing.");
     }
-    void show(){
+
+    void show() {
         System.out.println("Java is awesome.");
     }
 }
+
 class Computer {
     Computer() {
         System.out.println("Constructor of Computer class.");
     }
+
     void computer_method() {
         System.out.println("Power gone! Shut down your PC soon...");
     }
+
     public static void main(String[] args) {
         Computer my = new Computer();
         Laptop your = new Laptop();
@@ -172,10 +197,12 @@ class Computer {
         your.laptop_method();
     }
 }
+
 class Laptop {
     Laptop() {
         System.out.println("Constructor of Laptop class.");
     }
+
     void laptop_method() {
         System.out.println("99% Battery available.");
     }
@@ -195,12 +222,15 @@ class Programming {
 
 class Language {
     String name;
+
     Language() {
         System.out.println("Constructor method called.");
     }
+
     Language(String t) {
         name = t;
     }
+
     public static void main(String[] args) {
         Language cpp = new Language();
         Language java = new Language("Java");
@@ -208,9 +238,11 @@ class Language {
         java.getName();
         cpp.getName();
     }
+
     void setName(String t) {
         name = t;
     }
+
     void getName() {
         System.out.println("Language name: " + name);
     }
@@ -221,7 +253,6 @@ class Division {
         int a, b, result;
         Scanner input = new Scanner(System.in);
         System.out.println("Input two integers");
-
 
 
         a = input.nextInt();
@@ -237,16 +268,172 @@ class Division {
         }
     }
 }
- class TestThrow1{
-    static void validate(int age){
-        if(age<18)
+
+class TestThrow1 {
+    static void validate(int age) {
+        if (age < 18)
             throw new ArithmeticException("not valid");
         else
             System.out.println("welcome to vote on Technolamror");
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         validate(13);
         System.out.println("rest of the code...");
     }
 }
 
+class Allocate {
+    public static void main(String[] args) {
+        try {
+            long data[] = new long[1000000000];
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("finally block will execute always.");
+        }
+    }
+}
+
+interface Info {
+    static final String language = "Java";
+
+    public void display();
+}
+
+class Simple implements Info {
+    public static void main(String[] args) {
+        Simple obj = new Simple();
+        obj.display();
+    }
+
+    // Defining method declared in interface
+    public void display() {
+        System.out.println(language + " is awesome");
+    }
+}
+
+class SQLDateExample {
+    public static void main(String[] args) {
+        long millis = System.currentTimeMillis();
+        java.sql.Date date = new java.sql.Date(millis);
+        System.out.println(date);
+    }
+}
+/// Another Way
+
+class GetCurrentDateAndTime {
+    public static void main(String args[]) {
+        int day, month, year;
+        int second, minute, hour;
+        GregorianCalendar date = new GregorianCalendar();
+        day = date.get(Calendar.DAY_OF_MONTH);
+        month = date.get(Calendar.MONTH);
+        year = date.get(Calendar.YEAR);
+        second = date.get(Calendar.SECOND);
+        minute = date.get(Calendar.MINUTE);
+        hour = date.get(Calendar.HOUR);
+
+
+        System.out.println("Current date is " + day + "/" + (month + 1) + "/" + year);
+        System.out.println("Current time is " + hour + " : " + minute + " : " + second);
+    }
+}
+
+
+class SimpleDateFormatExample2 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Date formate chnage by Technolamror ");
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        String strDate = formatter.format(date);
+        System.out.println("Date Format with MM/dd/yyyy : " + strDate);
+
+        formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with dd-M-yyyy hh:mm:ss : " + strDate);
+
+        formatter = new SimpleDateFormat("dd MMMM yyyy");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with dd MMMM yyyy : " + strDate);
+
+        formatter = new SimpleDateFormat("dd MMMM yyyy zzzz");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with dd MMMM yyyy zzzz : " + strDate);
+
+        formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with E, dd MMM yyyy HH:mm:ss z : " + strDate);
+    }
+}
+
+class RandomNumbers {
+    public static void main(String[] args) {
+        int c;
+        Random t = new Random();
+        // random integers in [0, 100]
+        for (c = 1; c <= 10; c++) {
+            System.out.println(t.nextInt(100));
+        }
+    }
+}
+
+class GarbageCollection {
+    public static void main(String s[]) throws Exception {
+        Runtime rs = Runtime.getRuntime();
+        System.out.println("Free memory in JVM before Garbage Collection = " + rs.freeMemory());
+        rs.gc();
+        System.out.println("Free memory in JVM after Garbage Collection =" + rs.freeMemory());
+    }
+}
+
+class IPAddress {
+    public static void main(String args[]) throws Exception {
+        System.out.println(InetAddress.getLocalHost());
+    }
+}
+
+class Notepad {
+    public static void main(String[] args) {
+        Runtime rs = Runtime.getRuntime();
+        try {
+            rs.exec("notepad");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class LinearSearch {
+    public static void main(String args[]) {
+        int c, n, search, array[];
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number of elements");
+        n = in.nextInt();
+        array = new int[n];
+        System.out.println("Enter " + n + " integers");
+        for (c = 0; c < n; c++)
+            array[c] = in.nextInt();
+        System.out.println("Enter value to find");
+        search = in.nextInt();
+        for (c = 0; c < n; c++) {
+            if (array[c] == search) /* Searching element is present */ {
+                System.out.println(search + " is present at location " + (c + 1) +
+                        ".");
+                break;
+            }
+
+
+        }
+        if (c == n) /* Searching element is absent */
+            System.out.println(search + " is not present in array.");
+    }
+}
+
+ class StringToSQLDateExample {
+    public static void main(String[] args) {
+        String str="2015-03-31";
+        long date= Date.parse(str);//converting string into sql date
+        System.out.println(date);
+    }
+}
