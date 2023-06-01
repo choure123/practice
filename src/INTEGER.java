efa07f5c7eedf2ca7148a70ade80c1ac34eab3db
sonu choure <131000134+choure123@users.noreply.github.com>
2023-06-01 19:08:50 +0530

    ADDED CONDITION

--- src/INTEGER.java
+++ src/INTEGER.java
@@ -184,3 +184,17 @@ class Largestofthreenumber{
 
     }
 }
+class Condition{
+    public static void main(String[] args) {
+        boolean learning = true;
+        if (learning) {
+
+            System.out.println("java program");
+        }
+        else {
+            System.out.println( "what are  you donig here ");
+
+        }
+
+    }
+}
